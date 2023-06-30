01d4ff24b2c1bb1bb2ac185dec34c8f31b806f96
juliojesus15 <juliojesus1508@outlook.com>
2023-06-30 00:24:52 -0500

    ADD: Funcionalidad para obtener links en un rango de semestres

--- api/app/controllers/link_controller.py
+++ api/app/controllers/link_controller.py
@@ -5,15 +5,19 @@ class LinkController:
 		self.link_service = link_service
 		
 		self.blueprint = Blueprint('link', __name__)
-		self.blueprint.route('/link', methods=['GET'])(self.get_links)
+		self.blueprint.route('/link', methods=['GET'])(self.get_links_within_range)
 		
-	def get_links(self):
+		
+	def get_links_within_range(self):
 		try:				
 			semester_from = request.args.get('semester_from')
 			semester_to = request.args.get('semester_to')
-			filename = semester_from + "_" + semester_to			
-			message, links = self.link_service.get_links(filename)
-			return jsonify({ 'message': message, 'links': links }), 200
+
+			links = self.link_service.get_links_within_range(semester_from, semester_to)
+			
+			return jsonify({'status': True, 'links': links}), 200
+		
 		except Exception as e:
 			error_message = str(e)
-			return jsonify({'message': error_message, 'links': None}), 500
+			return jsonify({ 'status': error_message, 'links': None }), 500
+
--- api/app/services/link_service.py
+++ api/app/services/link_service.py
@@ -1,13 +1,34 @@
+from utils.semester import get_semesters_within_range
+
 class LinkService:
   def __init__(self, link_repository):
     self.link_repository = link_repository
 
-  def get_links(self, filename):
-    try:
-      links = self.link_repository.get_links(filename)
-      if links is None:
-        raise Exception('No se encontraron links para el periodo especificado')
-      return links
-    except Exception as e:
-      raise Exception('Error en el servicio de links: ' + str(e))
+  def get_links_within_range(self, semester_from, semester_to):
+    """
+      Obtiene los links dentro de un rango de semestres.
+      Args:
+        semester_from (str): Semestre inicial del rango.
+        semester_to (str): Semestre final del rango.
+      Returns:
+        lista de links concatenados.
+    """
 
+    try:
+      semesters = get_semesters_within_range(semester_from, semester_to)
+      combined_semesters = [f"{x}_{y}" for x, y in zip(semesters, semesters[1:])]
+      
+      container_links = []
+      
+      for semester in combined_semesters:
+        message, links = self.link_repository.get_links(semester)
+        container_links.append(links)
+      
+        if links is None:
+          raise Exception('No se encontraron links para el rango especificado')      
+        
+      joined_links = [link for sublist in container_links for link in sublist]          
+      return joined_links
+    
+    except Exception as e:
+      raise Exception('Error en el servicio de nodos: ' + str(e))
